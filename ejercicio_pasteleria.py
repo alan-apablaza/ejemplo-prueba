eeef1585ce1e1385418e93a3382aba5d2eedfe58
alan-apablaza <al.apablazad@duocuc.cl>
2026-07-15 01:36:20 -0400

    agregando diccionario

---
+++ ejercicio_pasteleria.py
@@ -0,0 +1,12 @@
+# productos = {codigo: [tipo, nombre, sabor, tamano, vegano], ...}
+
+productos = {
+    'TRT01': ['Torta',   'Selva Negra',    'Chocolate',   'Grande',     'No'],
+    'TRT02': ['Torta',   'Tres Leches',    'Vainilla',    'Mediana',    'No'],
+    'GAL01': ['Galleta', 'Chips de Choco', 'Chocolate',   'Individual', 'No'],
+    'GAL02': ['Galleta', 'Avena y Pasas',  'Avena',       'Individual', 'Si'],
+    'PAN01': ['Pan',     'Baguette',       'Natural',     'Grande',     'Si'],
+    'PAN02': ['Pan',     'Croissant',      'Mantequilla', 'Individual', 'No'],
+    'BEB01': ['Bebida',  'Cafe Latte',     'Cafe',        'Mediano',    'No'],
+    'BEB02': ['Bebida',  'Jugo Natural',   'Naranja',     'Grande',     'Si'],
+}
